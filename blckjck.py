9ef0cb608d89c40fd6baebc63617eaacb18bcf02
wdodd <wil.d@live.co.uk>
2016-09-07 16:11:46 +0100

    blckjck.py

--- blckjck.py
+++ blckjck.py
@@ -1,34 +1,59 @@
 import random
 
 def createDeck():
-	deck = []
-	suit = ["h","s","c","d"]
-	card = []
-	for x in range(len(suit)):
-		for y in range(0,14):
-			card = [suit[x],y]
-			deck.append(card)
-	return deck
-	
+        deck = []
+        suit = ["h","s","c","d"]
+        card = []
+        for x in range(len(suit)):
+                for y in range(0,14):
+                        card = [suit[x],y]
+                        deck.append(card)
+        return deck
+        
 def shuffle(deck):
-	random.shuffle(deck)
-	return deck
+        random.shuffle(deck)
+        return deck
 
 def play(deck):
-	score = 0
-	play = True
-	x = 1
-	while play == True:
-		card = deck[x]
-		print card
-		print card[1]
-		score = score + card[1]
-		print score
-		x = x + 1
-		hs = raw_input("hit or stay")
-		if hs == "h":
-			print "hit"
-		elif hs == "s":
-			print "stay"
-			print score
-			break
+        score = 0
+        play = True
+        x = 1
+        while play == True:
+                newCard = drawCard(deck, x)
+                score = score + newCard
+                print score
+                x = x + 1
+                if score > 21:
+                        print "out too high!"
+                        break
+                elif score == 21:
+                        print "blackjack bitch"
+                        break
+                hs = raw_input("hit or stay")
+                if hs == "h":
+                        print "Hit Me!"
+                elif hs == "s":
+                        #print "stay"
+                        print "Final Score:", score
+                        break
+def drawCard(deck, x):
+        card = deck[x]
+        print "Card Drawn:",card[1]
+        if card[1] > 10:
+            newCard = 10
+        else:
+            newCard = card[1]
+        return newCard
+
+def main():
+    print "Welcome to Blackjack!"
+    cont = True
+    while cont == True:
+        play(shuffle(createDeck()))
+        again = raw_input("Play again? [y/n]")
+        if again == "n":
+            print "Exiting Game"
+            break
+        
+if __name__ == "__main__":
+    main()
